9ba0b161e323f01f2d3fb9a5e542e3d0041919f3
Fabian Mast <fabian.mast@gmx.de>
2023-10-02 18:08:14 -0600

    specify password saving method

--- SEL4C/settings.py
+++ SEL4C/settings.py
@@ -90,6 +90,10 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
