60297ce8bee6def9be9e2d1de572df820da0b494
Fabian Mast <fabian.mast@gmx.de>
2023-10-02 19:28:36 -0600

    update csrf origins

--- SEL4C/settings.py
+++ SEL4C/settings.py
@@ -29,7 +29,9 @@ ALLOWED_HOSTS = ['sel4c.azurewebsites.net',
                  '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['http://sel4c.azurewebsites.net',
-                 'http://127.0.0.1']
+                        'https://sel4c.azurewebsites.net',
+                 'http://127.0.0.1',
+                 'https://127.0.0.1']
 
 # Application definition
 
