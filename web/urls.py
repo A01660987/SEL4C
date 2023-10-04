a1814d7321ee2ed96a518bb09f0630b7d7dd1744
Diego Figueroa <a01660987@tec.mx>
2023-10-04 02:39:01 -0600

    Pequeñas correcciones, mejoras al landing

--- web/urls.py
+++ web/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("login", views.login, name="login"),
-    path("about", views.about, name="about"),
+    path("login/", views.login, name="login"),
+    path("about/", views.about, name="about"),
     path("about/team", views.aboutTeam, name="aboutTeam"),
-    path("register", views.register, name="register"),
-    path("dashboard/", views.admin_panel, name="admin_panel"),
+    path("register/", views.register, name="register"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
