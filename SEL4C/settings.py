e397c09871428c24f081c540ed01422231fcd3d1
Diego Figueroa <a01660987@tec.mx>
2023-10-07 04:22:18 -0600

    Update settings.py

--- SEL4C/settings.py
+++ SEL4C/settings.py
@@ -98,8 +98,15 @@ WSGI_APPLICATION = 'SEL4C.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "sel4c-database",
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "require",
+        }
     }
 }
 
