a1953e308dcb1fc46ba38d9364f07997cf5f4fb3
Diego Figueroa <a01660987@tec.mx>
2023-10-07 01:51:43 -0600

    Update settings.py

--- SEL4C/settings.py
+++ SEL4C/settings.py
@@ -146,12 +146,6 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
