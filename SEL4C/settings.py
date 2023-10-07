409d4c3ebbaf087d2ae34173101229493b6499f2
Diego Figueroa <a01660987@tec.mx>
2023-10-07 01:05:08 -0600

    Cambiar archivos estáticos de 'static' a 'staticfiles'

--- SEL4C/settings.py
+++ SEL4C/settings.py
@@ -144,10 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
-    "static": {
+    "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
