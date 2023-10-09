815f31e55cc53e2d00f1fcadffeb8dcb79621abe
Fabian Mast <fabian.mast@gmx.de>
2023-10-09 17:25:48 -0600

    added useradmin

--- web/admin.py
+++ web/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group as authGroup
 from SEL4C.models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+class UserAdmin(BaseUserAdmin):
+
+    model = User
+    # extra = 1
 
 admin.site.unregister(authGroup)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Group)
 admin.site.register(Institution)
 admin.site.register(Discipline)
