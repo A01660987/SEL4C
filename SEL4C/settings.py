206acaf5ba63d7fa9ff2d29ff2bf147e49e6ec7b
Diego Figueroa Peart <a01660987@tec.mx>
2023-10-09 21:17:14 -0600

    Update home.html + settings.py

--- SEL4C/settings.py
+++ SEL4C/settings.py
@@ -114,12 +114,12 @@ DATABASES = {
 }
 
 # Descomentar para pruebas locales
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
