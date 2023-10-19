02a0109736d404107a20e2c5744dab492ef7b9e3
Emiliano Vivas Rodríguez <vivasrguez@outlook.com>
2023-10-19 13:39:06 -0600

    Charts

--- web/urls.py
+++ web/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("student/dashboard/", views.student_dashboard, name="student_dashboard"),
+
+    path("student_gender_chart/", views.student_gender_chart, name="student_gender_chart"),
+    path("student_age_chart/", views.student_age_chart, name="student_age_chart"),
+    path("student_country_chart/", views.student_country_chart, name="student_country_chart"),
+    path("student_degree_chart/", views.student_degree_chart, name="student_degree_chart"),
+    path("student_discipline_chart/", views.student_discipline_chart, name="student_discipline_chart"),
+    path("student_institution_chart/", views.student_institution_chart, name="student_institution_chart"),
+    path("answer_activity_chart/", views.answer_activity_chart, name="answer_activity_chart"),
 ]
--- web/views.py
+++ web/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from SEL4C.models import *
-
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 def home(request):
     return render(request, "home.html")
@@ -50,3 +50,112 @@ def student_dashboard(request):
     user = User.objects.get(username=request.user.username)
     student = Student.objects.get(user=user)
     return render(request, "student-dashboard.html", {"student": student,})
+
+@xframe_options_exempt
+def student_gender_chart(request):
+    labels = ["Masculino", "Femenino", "No binario", "Otro", "Prefiero no decir"]
+    data = [Student.objects.filter(gender = 'M').count(), 
+            Student.objects.filter(gender = 'F').count(),
+            Student.objects.filter(gender = 'N').count(),
+            Student.objects.filter(gender = 'O').count(),
+            Student.objects.filter(gender = 'P').count()]
+    return render(request, "barchart.html", {
+        "labels": labels, 
+        "data": data
+    })
+
+@xframe_options_exempt
+def student_age_chart(request):
+    age_list = list(Student.objects.values('age').all())
+    ages = []
+    age_students = []
+    for age in age_list:
+        ages.append(str(age['age']) + " años")
+        age_students.append(Student.objects.filter(age = age['age']).count())
+
+    labels = ages
+    data = age_students
+    return render(request, "barchart.html", {
+        "labels": labels, 
+        "data": data
+    })
+
+@xframe_options_exempt
+def student_country_chart(request):
+    country_list = list(Student.objects.values('country').all())
+    countries = []
+    country_students = []
+    for country in country_list:
+        countries.append(country['country'])
+        country_students.append(Student.objects.filter(country = country['country']).count())
+
+    labels = countries
+    data = country_students
+    return render(request, "barchart.html", {
+        "labels": labels, 
+        "data": data
+    })
+
+@xframe_options_exempt
+def student_degree_chart(request):
+    degree_list = list(Degree.objects.values('id', 'type').all())
+    degrees = []
+    degree_students = []
+    for degree in degree_list:
+        degrees.append(degree['type'])
+        degree_students.append(Student.objects.filter(degree = degree['id']).count())
+
+    labels = degrees
+    data = degree_students
+    return render(request, "piechart.html", {
+        "labels": labels, 
+        "data": data
+    })
+
+@xframe_options_exempt
+def student_discipline_chart(request):
+    discipline_list = list(Discipline.objects.values('id', 'name').all())
+    disciplines = []
+    discipline_students = []
+    for discipline in discipline_list:
+        disciplines.append(discipline['name'])
+        discipline_students.append(Student.objects.filter(discipline = discipline['id']).count())
+
+    labels = disciplines
+    data = discipline_students
+    return render(request, "piechart.html", {
+        "labels": labels, 
+        "data": data
+    })
+
+@xframe_options_exempt
+def student_institution_chart(request):
+    institution_list = list(Institution.objects.values('id', 'name').all())
+    institutions = []
+    institution_students = []
+    for institution in institution_list:
+        institutions.append(institution['name'])
+        institution_students.append(Student.objects.filter(institution = institution['id']).count())
+
+    labels = institutions
+    data = institution_students
+    return render(request, "piechart.html", {
+        "labels": labels, 
+        "data": data
+    })
+
+@xframe_options_exempt
+def answer_activity_chart(request):
+    activity_list = list(Answer.objects.values('activity').all())
+    answers = []
+    answer_activities = []
+    for activity in activity_list:
+        answers.append(activity['activity'])
+        answer_activities.append(Answer.objects.filter(activity = activity['activity']).count())
+
+    labels = answers
+    data = answer_activities
+    return render(request, "piechart.html", {
+        "labels": labels, 
+        "data": data
+    })
